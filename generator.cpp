6fe9547ed3385a874fd3c9d74b6e71f763a6c903
Aldo Culquicondor <alculquicondor@gmail.com>
2015-06-15 17:25:22 -0500

    Defining type NodeId

--- generator.cpp
+++ generator.cpp
@@ -6,20 +6,22 @@
 
 using namespace std;
 
+typedef int NodeId;
+
 int main(int argc, char *argv[]) {
   srand((unsigned int)time(nullptr));
   if (argc < 3)
     exit(1);
-  long n = atol(argv[1]);
+  int n = atoi(argv[1]);
   int max_degree = atoi(argv[2]);
-  vector<long> vertices(n);
-  vector<long> edges;
-  for (long u = 0; u < vertices.size(); ++u) {
-    vertices[u] = edges.size();
-    set<long> used;
+  vector<NodeId> vertices((size_t)n);
+  vector<NodeId> edges;
+  for (NodeId u = 0; u < n; ++u) {
+    vertices[u] = (NodeId)edges.size();
+    set<NodeId> used;
     int t = rand() % (max_degree + 1);
     for (int i = 0; i < t; ++i) {
-      long v;
+      NodeId v;
       do {
         v = rand() % n;
       } while (v == u || used.find(v) != used.end());
@@ -27,12 +29,12 @@ int main(int argc, char *argv[]) {
     }
     edges.insert(edges.end(), used.begin(), used.end());
   }
-  printf("%ld %ld\n", n, edges.size());
-  for (long u : vertices)
-    printf(" %ld", u);
+  printf("%u %u\n", n, (NodeId)edges.size());
+  for (NodeId u : vertices)
+    printf(" %u", u);
   puts("");
-  for (long u : edges)
-    printf(" %ld", u);
+  for (NodeId u : edges)
+    printf(" %u", u);
   puts("");
   return 0;
 }
